3a4263835ff7b37dad6f40bdf7cef0158c882cb9
SHIV2404 <93988219+SHIV2404@users.noreply.github.com>
2021-11-13 23:11:47 +0530

    updated axios issue

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./Countries.css";
 import { Link } from "react-router-dom";
 import "aos/dist/aos.css";
